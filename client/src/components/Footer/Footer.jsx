ee25cf55203290051028be8ccc1079d3d220bf3d
João Vítor <32188614+JoaovitorFerreira@users.noreply.github.com>
2022-07-25 11:17:15 -0300

    Update Footer.jsx

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,6 +4,6 @@ import style from './Footer.styles'
 export default function Footer() {
     const useStyles = style
     return (
-        <div style={useStyles}><p>Exemplo criado para @Genial Investimentos</p></div>
+        <div style={useStyles}><p>Exemplo criado por João Vítor Ferreira</p></div>
     )
 }
